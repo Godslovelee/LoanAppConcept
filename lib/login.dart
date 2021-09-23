61499e0421304e22cb1c43dc3e86fbdac798482c
Godslovelee <godslovelee@yahoo.com>
2021-09-23 14:15:30 +0100

    "error-handling"

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+
 class LoginPage extends StatelessWidget {
   //const LoginPage({Key key}) : super(key: key);
   String _email;
@@ -8,69 +9,67 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login Basic"),
-      ),
+        appBar: AppBar(
+          title: Text("Login Basic"),
+        ),
         body: Container(
-          padding: EdgeInsets.all(16.0),
-          child: Form(
-
-            key: formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(labelText: "email"),
-                  //saving email and password state
-                  onSaved: (value) => _email = value,
-                  validator: (value) => value.isEmpty ? "Email Field is empty" : null,
-
-                ),
-                TextFormField(
-                  onSaved: (data) => _password= data,
-                  validator: (data) => data.isEmpty ? "Password cant be empty" : null,
-                  decoration: InputDecoration(labelText: "password"),
-                  obscureText: true,
-                ),
-                FloatingActionButton(
-                    child: Text("Login", style: TextStyle(fontSize: 20.0),)
-                    ,
-                    onPressed: () => validateAndSubmit()
-
-                )
-
-              ],
-            ),
-          )
-        ));
-
-
+            padding: EdgeInsets.all(16.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(labelText: "email"),
+                    //saving email and password state
+                    onSaved: (value) => _email = value,
+                    validator: (value) =>
+                        value.isEmpty ? "Email Field is empty" : null,
+                  ),
+                  TextFormField(
+                    onSaved: (data) => _password = data,
+                    validator: (data) =>
+                        data.isEmpty ? "Password cant be empty" : null,
+                    decoration: InputDecoration(labelText: "password"),
+                    obscureText: true,
+                  ),
+                  FloatingActionButton(
+                      child: Text(
+                        "Login",
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                      onPressed: () => validateAndSubmit())
+                ],
+              ),
+            )));
   }
-  bool validateAndSave() {
 
+  bool validateAndSave() {
     final form = formKey.currentState;
-    if(form.validate()){
+    if (form.validate()) {
       form.save();
       return true;
       //print("Input is vaild, Email: $_email, $_password");
-    }
-    else{
+    } else {
       return false;
       //print("Input invalid, Email: $_email, $_password");
     }
-
   }
 
-  validateAndSubmit()async{
-    if(validateAndSave()){
+  validateAndSubmit() async {
+    if (validateAndSave()) {
+      try{
+        FirebaseUser user = await FirebaseAuth.instance
+            .signInWithEmailAndPassword(email: _email, password: _password);
+        print("Login with password with  + ${user.uid}");
 
-      FirebaseUser user = await
-        FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password);
+      }
+      catch(er){
+        print("Error: $er");
+
+      }
 
 
     }
   }
-
 }
-
