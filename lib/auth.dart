f904126238c88771ab6a9efd6fd09fbc9f0a92bb
Godslovelee <godslovelee@yahoo.com>
2021-10-05 05:44:43 +0100

    ""

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,19 +1,25 @@
-
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'dart:async';
 
 abstract class BaseAuthFireBase{
   Future<String>signInWithEmailAndPassword(email, password);
+  Future<String>createUserWithEmailAndPassword(email, password);
 }
 
 class Auth implements BaseAuthFireBase{
 Future<String>signInWithEmailAndPassword(email, password)async{
   FirebaseUser user = (await FirebaseAuth.instance
       .signInWithEmailAndPassword(
-      email: email, password: password)) as FirebaseUser;
+      email: email, password: password)).user;
   return user.uid;
 
+}
+
+Future<String>createUserWithEmailAndPassword(email, password)async{
+  FirebaseUser user = (await FirebaseAuth.instance
+      .createUserWithEmailAndPassword(
+      email: email, password: password)).user;
+  return user.uid;
 }
 
 
